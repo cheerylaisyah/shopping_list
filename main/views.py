73e82ea78822d5ffa6cfb554fe42387d7f8f904e
cheerylaisyah <cheeryl.aisyah@ui.ac.id>
2023-09-21 21:42:23 +0700

    delete env

--- main/views.py
+++ main/views.py
@@ -60,8 +60,8 @@ def show_main(request):
     return render(request, "main.html", context)
 
 def create_product(request):
-     form = ProductForm(request.POST or None)
-     if form.is_valid() and request.method == "POST":
+    form = ProductForm(request.POST or None)
+    if form.is_valid() and request.method == "POST":
         product = form.save(commit=False)
         product.user = request.user
         product.save()
